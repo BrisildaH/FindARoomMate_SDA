f41e969551d67912e788efa93939da95ac200cfc
BrisildaH <brisildahysaj@gmail.com>
2024-03-09 15:51:02 +0100

    Finall work

--- BusinessLayer/ApplicationService.cs
+++ BusinessLayer/ApplicationService.cs
@@ -15,20 +15,95 @@ namespace FindMeARoomate.BusinessLayer
             return applications;
         }
         //Make an application
-        public void MakeApplication()
+        public void MakeApplication(string title, string email)
         {
-            Console.WriteLine("Make an Application");
             var application = new Application();
-            Console.WriteLine("Enter Announcement ID");
-            application.AnnouncementID = int.Parse(Console.ReadLine());
+            var announcementRepository = new AnnouncementRepository();
+
+            Console.WriteLine("Make an Application");
+
+            application.Status = "new";
+            application.ApplicationDate = DateTime.Now;
+            application.IsActive = true;
+
+            var studentRepository = new StudentRepository();
+
+            Console.WriteLine("Title of announcement");
+            var announcement1 = announcementRepository.GetAllAnnouncement()
+                                    .Where(p => p.Title == title)
+                                     .FirstOrDefault();
+
+
+            if (announcement1 == null)
+            {
+                Console.WriteLine("Announcement doesn't exist");
+                throw new Exception("Announcement doesn't exist");
+            }
+            else
+            {
+                application.AnnouncementID = announcement1.ID;
+            }
+
+            var student = studentRepository.GetAllStudent()
+                               .Where(p => p.Email == email)
+                                .FirstOrDefault();
+
+            if (student == null)
+            {
+                Console.WriteLine("Application not found");
+                throw new Exception("Application not found");
+            }
+
+            else
+            {
+                application.StudentID = student.Id;
+            }
 
             //Add student
             var applicationRepository = new ApplicationRepository();
             applicationRepository.AddApplication(application);
+
         }
+
+        //Application approved
+
+
         //Log in
         //Application Business Logic
         #endregion
+
+        public void ApprovedAppliation(int applicationID)
+        {
+
+            var applicationRepository = new ApplicationRepository();
+
+            var application = applicationRepository.GetAllApplication()
+                                   .Where(p => p.ID == applicationID)
+                                    .FirstOrDefault();
+
+            if (application == null)
+            {
+                Console.WriteLine("Application not found");
+            }
+
+            else
+            {
+                application.Status= "Approved";
+                applicationRepository.UpdateApplication(application);
+            }
+
+        }
     }
 }
+/*
+Task Crate Service Make an application
+Parameters(titullin e announcement, emailin e userit)
+titullin e announcement => application.AnnouncementID
+emailin e userit => application.ApplicantID*/
 
+/*
+Task Approvo Application
+Parameter Application ID
+Get Applicaiton
+Status New = Approved
+Repositoy.Update()*/
--- Program.cs
+++ Program.cs
@@ -2,6 +2,9 @@
 using FindMeARoomate.BusinessLayer;
 using FindMeARoomate.DataLayer.DatabaseContext;
 using FindMeARoomate.DataLayer.Entities;
+using System;
+using System.Transactions;
+
 Console.WriteLine("Welcome to Find Roommate Application!");
 Console.WriteLine("--------------------------------------------------");
 Console.WriteLine("Menu");
@@ -10,6 +13,9 @@ Console.WriteLine("2 - Get All students");
 Console.WriteLine("3 - Log In");
 Console.WriteLine("4 - Get My Profile");
 Console.WriteLine("5 - Make an announcement");
+Console.WriteLine("6 - Make an application");
+Console.WriteLine("7 - Approve an application");
+
 int choice = int.Parse(Console.ReadLine());
 switch (choice)
 {
@@ -44,6 +50,7 @@ switch (choice)
             studentService.LogIn(Email, Password);
             break;
         }
+
     case 4:
         {
             Console.WriteLine("Enter Email");
@@ -64,5 +71,33 @@ switch (choice)
         //Get My Profile
         break;
     }
+
+    case 6:
+        {
+            Console.WriteLine("Enter title");
+            var Title = Console.ReadLine();
+
+            Console.WriteLine("Enter email");
+            var Email = Console.ReadLine();
+
+            var applicationService = new ApplicationService();
+
+            applicationService.MakeApplication(Title, Email);
+        
+            break;
+        }
+
+        case 7:
+        {
+            Console.WriteLine("Enter ID");
+            var ApplicationID = int.Parse(Console.ReadLine());
+
+            var applicationService = new ApplicationService();
+            applicationService.ApprovedAppliation(ApplicationID);
+
+            Console.WriteLine("Approved application");
+
+            break;
+        }
 }
 Console.WriteLine("--------------------------------------------------");
